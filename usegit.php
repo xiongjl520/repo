f671b8c77c9669074c27274e77743e36b677b316
xjl <912097220@qq.com>
2017-02-21 11:38:43 +0800

    varsion 1.0

--- usegit.php
+++ usegit.php
@@ -1,4 +1,6 @@
 <?php
 	echo 'use git!';
-
+	echo '厉害了我的git!';
+	//查看git版本信息，回退版本，HEAD当前版本 ^的前n个^的版本，--hard可以根据log里面保存的commitid 回退到指定的版本
+	echo 'git log ;git reset --hard HEAD^ or HEAD~2 git reset --hard \'commit id\';
 ?>
