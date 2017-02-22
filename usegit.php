f76de257b07bd07f85976b10e38e36f05b6c3505
xjl <912097220@qq.com>
2017-02-22 13:41:16 +0800

    2017.2.22

--- usegit.php
+++ usegit.php
@@ -3,4 +3,5 @@
 	echo '厉害了我的git!';
 	//查看git版本信息，回退版本，HEAD当前版本 ^的前n个^的版本，--hard可以根据log里面保存的commitid 回退到指定的版本
 	echo 'git log ;git reset --hard HEAD^ or HEAD~2 git reset --hard \'commit id\';
+	//2017.2.22修改
 ?>
